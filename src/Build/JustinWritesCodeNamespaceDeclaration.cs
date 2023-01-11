90c63a04072b7523bd864cc0c384c84f25f47993
Justin <justin@justinwritescode.com>
2023-01-11 00:50:57 -0500

    Fixed the readme problem (I think!)

---
+++ src/Build/JustinWritesCodeNamespaceDeclaration.cs
@@ -0,0 +1,14 @@
+/* 
+ * JustinWritesCodeNamespace.cs
+ * 
+ *   Created: 2023-01-10-10:37:17
+ *   Modified: 2023-01-10-10:37:17
+ * 
+ *   Author: Justin Chase <justin@justinwritescode.com>
+ *   
+ *   Copyright © 2022-2023 Justin Chase, All Rights Reserved
+ *      License: MIT (https://opensource.org/licenses/MIT)
+ */ 
+
+/// <summary>The root namespace for (most) JustinWritesCode projects.</summary>
+namespace JustinWritesCode;
