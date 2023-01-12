e723031b8d924b20643a7d8ba69b02473c6ef83f
Justin <justin@justinwritescode.com>
2023-01-11 23:42:22 -0500

    changes

--- src/Build/JustinWritesCodeNamespaceDeclaration.cs
+++ src/Build/JustinWritesCodeNamespaceDeclaration.cs
@@ -1,14 +1,15 @@
-/* 
+/*
  * JustinWritesCodeNamespace.cs
- * 
+ *
  *   Created: 2023-01-10-10:37:17
  *   Modified: 2023-01-10-10:37:17
- * 
+ *
  *   Author: Justin Chase <justin@justinwritescode.com>
- *   
+ *
  *   Copyright © 2022-2023 Justin Chase, All Rights Reserved
  *      License: MIT (https://opensource.org/licenses/MIT)
- */ 
+ */
 
-/// <summary>The root namespace for (most) JustinWritesCode projects.</summary>
+global using JWC = global::JustinWritesCode;
+global using Jwc = global::JustinWritesCode;
 namespace JustinWritesCode;
