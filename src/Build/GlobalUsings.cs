77621e53dc113f4b39c3ca7e3ad5c905ee3a2dc7
Justin <justin@justinwritescode.com>
2023-01-14 04:29:14 -0500

    fixed that damn problem with the DLLs not showing up!

---
+++ src/Build/GlobalUsings.cs
@@ -0,0 +1,27 @@
+/*
+ * JustinWritesCodeNamespace.cs
+ *
+ *   Created: 2023-01-10-10:37:17
+ *   Modified: 2023-01-10-10:37:17
+ *
+ *   Author: Justin Chase <justin@justinwritescode.com>
+ *
+ *   Copyright © 2022-2023 Justin Chase, All Rights Reserved
+ *      License: MIT (https://opensource.org/licenses/MIT)
+ */
+
+namespace JustinWritesCode { }
+
+namespace System { }
+namespace System.Collections.Generic { }
+namespace System.Diagnostics { }
+namespace System.IO { }
+namespace System.Linq { }
+namespace System.Linq.Expressions { }
+namespace System.Reflection { }
+namespace System.Security.Claims { }
+namespace System.Text.RegularExpressions { }
+namespace System.Threading { }
+namespace System.Threading.Tasks { }
+namespace System.Diagnostics.CodeAnalysis { }
+namespace System.Net.Http { }
