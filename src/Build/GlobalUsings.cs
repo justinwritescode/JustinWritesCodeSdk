414d0bab0b6aaf7311d15aa3a60c7ab84de18388
Justin <justin@justinwritescode.com>
2023-01-18 08:32:15 -0500

    lucky number 13?

--- src/Build/GlobalUsings.cs
+++ src/Build/GlobalUsings.cs
@@ -10,12 +10,9 @@
  *      License: MIT (https://opensource.org/licenses/MIT)
  */
 
-<<<<<<< HEAD
 #pragma warning disable CD1607 // The file needs documentation headers.
 #pragma warning disable RCS1072 // Remove empty namespace declaration.
 
-=======
->>>>>>> 9310e46 ("initial" commit -_-)
 namespace JustinWritesCode { }
 
 namespace System { }
@@ -31,9 +28,6 @@ namespace System.Threading { }
 namespace System.Threading.Tasks { }
 namespace System.Diagnostics.CodeAnalysis { }
 namespace System.Net.Http { }
-<<<<<<< HEAD
 
 #pragma warning restore CD1607 // The file needs documentation headers.
 #pragma warning restore RCS1072 // Remove empty namespace declaration.
-=======
->>>>>>> 9310e46 ("initial" commit -_-)
